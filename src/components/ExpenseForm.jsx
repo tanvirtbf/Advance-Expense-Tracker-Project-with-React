4d181382e35a5c4f85a1835db7e4e26720514156
tanvirtbf <tanver018765@gmail.com>
2024-06-25 12:26:19 +0600

    input box clear after submit

--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -11,7 +11,12 @@ export default function ExpenseForm({ setExpenses }) {
   const handleSubmit = (e) => {
     e.preventDefault();
     setExpenses((prevState) => [...prevState, data])
-    
+    setData((prevState)=>({
+      ...prevState,
+      title:"",
+      category:"",
+      amount:""
+    }))
   };
 
 
