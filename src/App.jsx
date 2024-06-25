db834d4aab722457d3a3b2b66ef3ede2b284119a
tanvirtbf <tanver018765@gmail.com>
2024-06-25 10:53:00 +0600

    formdata merge with real state data

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
       <main>
         <h1>Track Your Expense</h1>
         <div className="expense-tracker">
-          <ExpenseForm />
+          <ExpenseForm setExpenses={setExpenses}/>
           <ExpenseTable expenses={expenses}/>
         </div>
       </main>
--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -1,19 +1,23 @@
-export default function ExpenseForm() {
+export default function ExpenseForm({ setExpenses }) {
   const handleSubmit = (e) => {
     e.preventDefault();
-    const formData = new FormData(e.target)
-    const data = {}
-    for(const [key,value] of formData.entries()){
-      data[key] = value
+    const formData = new FormData(e.target);
+    const data = {};
+    for (const [key, value] of formData.entries()) {
+      data[key] = value;
     }
-    console.log(data)
+    console.log(data);
+    setExpenses((prevState) => [
+      ...prevState,
+      { id: crypto.randomUUID(), ...data },
+    ]);
   };
 
   return (
     <form className="expense-form" onSubmit={handleSubmit}>
       <div className="input-container">
         <label htmlFor="title">Title</label>
-        <input id="title" name="title"/>
+        <input id="title" name="title" />
       </div>
       <div className="input-container">
         <label htmlFor="category">Category</label>
@@ -30,7 +34,7 @@ export default function ExpenseForm() {
       </div>
       <div className="input-container">
         <label htmlFor="amount">Amount</label>
-        <input id="amount" name="amount"/>
+        <input id="amount" name="amount" />
       </div>
       <button className="add-btn">Add</button>
     </form>
