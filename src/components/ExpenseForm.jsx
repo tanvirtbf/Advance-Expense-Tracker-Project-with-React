c314b4a532359101cc046c9c702d94d368d6bae8
tanvirtbf <tanver018765@gmail.com>
2024-06-25 10:22:52 +0600

    expense form category input change and it's select

--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -9,7 +9,14 @@ export default function ExpenseForm() {
       </div>
       <div className="input-container">
         <label htmlFor="category">Category</label>
-        <input id="category" />
+        <select id="category">
+          <option value="">All</option>
+          <option value="grocery">Grocery</option>
+          <option value="clothes">Clothes</option>
+          <option value="bills">Bills</option>
+          <option value="education">Education</option>
+          <option value="medicine">Medicine</option>
+        </select>
       </div>
       <div className="input-container">
         <label htmlFor="amount">Amount</label>
--- src/components/ExpenseTable.jsx
+++ src/components/ExpenseTable.jsx
@@ -56,3 +56,4 @@ export default function ExpenseTable({ expenses }) {
     </table>
   );
 }
+
