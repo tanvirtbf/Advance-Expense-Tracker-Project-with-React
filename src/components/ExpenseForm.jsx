a5f1a6db9221de54f77e9b501df1ef9bbe756b8c
tanvirtbf <tanver018765@gmail.com>
2024-06-27 15:10:18 +0600

    let's create formData using onChange

--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -1,61 +1,18 @@
-import { useEffect, useRef, useState } from "react";
 import ErrorMsg from "./ErrorMsg";
 
 //Validation
 export default function ExpenseForm({ setExpenses }) {
-  const [isNull, setIsNull] = useState({
-    isTitle: true,
-    isCategory: true,
-    isAmount: true,
-  });
-
-  const titleRef = useRef("");
-  const categoryRef = useRef("");
-  const amountRef = useRef("");
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    useEffect(()=>{
-      console.log(titleRef.current.value);
-      console.log(categoryRef.current.value);
-      console.log(amountRef.current.value);
-      if (titleRef.current.value === "")
-        setIsNull((prev) => ({ ...prev, isTitle: false }));
-      if (categoryRef.current.value === "")
-        setIsNull((prev) => ({ ...prev, isCategory: false }));
-      if (amountRef.current.value === "")
-        setIsNull((prev) => ({ ...prev, isAmount: false }));
-  
-      let data = {
-        id: crypto.randomUUID(),
-        title: titleRef.current.value,
-        category: categoryRef.current.value,
-        amount: amountRef.current.value,
-      };
-  
-      if (
-        isNull.isTitle === true &&
-        isNull.isCategory === true &&
-        isNull.isAmount === true
-      ) {
-        setExpenses((prevState) => [...prevState, data]);
-        // titleRef.current.value = "";
-        // categoryRef.current.value = "";
-        // amountRef.current.value = "";
-      }
-    },[])
-  };
+  const handleSubmit = (e) => {};
 
   return (
     <form className="expense-form" onSubmit={handleSubmit}>
       <div className="input-container">
         <label htmlFor="title">Title</label>
-        <input id="title" ref={titleRef} />
-        {!isNull.isTitle && <ErrorMsg errorMsg={"Title Required"} />}
+        <input id="title"  />
       </div>
       <div className="input-container">
         <label htmlFor="category">Category</label>
-        <select id="category" ref={categoryRef}>
+        <select id="category" >
           <option value="" hidden>
             Select Category
           </option>
@@ -65,12 +22,10 @@ export default function ExpenseForm({ setExpenses }) {
           <option value="education">Education</option>
           <option value="medicine">Medicine</option>
         </select>
-        {!isNull.isCategory && <ErrorMsg errorMsg={"Category Required"} />}
       </div>
       <div className="input-container">
         <label htmlFor="amount">Amount</label>
-        <input id="amount" ref={amountRef} />
-        {!isNull.isAmount && <ErrorMsg errorMsg={"Amount Required"} />}
+        <input id="amount"  />
       </div>
       <button type="submit" className="add-btn">
         Add
