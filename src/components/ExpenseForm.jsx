b235f82ccbf8271f546ce96d91cf31bd8198293e
tanvirtbf <tanver018765@gmail.com>
2024-06-27 15:20:11 +0600

    now try alone

--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -2,28 +2,41 @@ import { useState } from "react";
 
 //Validation
 export default function ExpenseForm({ setExpenses }) {
-  const [expense,setExpense] = useState({
-    title:'',
-    category:'',
-    amount:'',
-  })
+  const [expense, setExpense] = useState({
+    title: "",
+    category: "",
+    amount: "",
+  });
   const handleSubmit = (e) => {
     e.preventDefault();
-    setExpenses((prevState)=>[
+    setExpenses((prevState) => [
       ...prevState,
-      {id:crypto.randomUUID(),...expense}
-    ])
+      { id: crypto.randomUUID(), ...expense },
+    ]);
   };
 
   return (
     <form className="expense-form" onSubmit={handleSubmit}>
       <div className="input-container">
         <label htmlFor="title">Title</label>
-        <input id="title" onChange={(e)=> setExpense((prevState)=>({...prevState,title:e.target.value}))} />
+        <input
+          id="title"
+          onChange={(e) =>
+            setExpense((prevState) => ({ ...prevState, title: e.target.value }))
+          }
+        />
       </div>
       <div className="input-container">
         <label htmlFor="category">Category</label>
-        <select id="category" onChange={(e)=> setExpense((prevState)=>({...prevState,category:e.target.value}))}>
+        <select
+          id="category"
+          onChange={(e) =>
+            setExpense((prevState) => ({
+              ...prevState,
+              category: e.target.value,
+            }))
+          }
+        >
           <option value="" hidden>
             Select Category
           </option>
@@ -36,7 +49,15 @@ export default function ExpenseForm({ setExpenses }) {
       </div>
       <div className="input-container">
         <label htmlFor="amount">Amount</label>
-        <input id="amount" onChange={(e)=>setExpense((prevState)=>({...prevState,amount:e.target.value}))} />
+        <input
+          id="amount"
+          onChange={(e) =>
+            setExpense((prevState) => ({
+              ...prevState,
+              amount: e.target.value,
+            }))
+          }
+        />
       </div>
       <button type="submit" className="add-btn">
         Add
