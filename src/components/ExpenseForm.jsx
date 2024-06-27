b5bc196435e1f8c5b46cd79f92acf4c0d7017132
tanvirtbf <tanver018765@gmail.com>
2024-06-27 15:55:23 +0600

    again validation part task start with alone

--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -1,51 +1,21 @@
 import { useState } from "react";
 
 export default function ExpenseForm({ setExpenses }) {
-  const [expense,setExpense] = useState({
-    title:'',
-    category:'',
-    amount:'',
-  })
-  const [errors,setErrors] = useState("")
-
-  const validate = (formData)=>{
-    let errorMsg = {}
-    if(!formData.title) errorMsg.title="Title Required"
-    if(!formData.category) errorMsg.category="Category Required"
-    if(!formData.amount) errorMsg.amount="Amount Required"
-
-    setErrors(errorMsg)
-    return errorMsg
-  }
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const validation = validate(expense)
-    if(Object.keys(validation).length) return 
-    setExpenses((prevState)=>[
-      ...prevState,
-      {id:crypto.randomUUID(),...expense}
-    ])
-  };
-
-  const handleChange = (e)=>{
-    const {name,value} = e.target
-    setExpense((prevState)=>({...prevState,[name]:value}))
-    setErrors({})
   }
 
   return (
     <form className="expense-form" onSubmit={handleSubmit}>
       <div className="input-container">
         <label htmlFor="title">Title</label>
-        <input id="title" name="title" value={expense.title} onChange={handleChange} />
-        {
-          errors.title && <span style={{color:'red'}}>{errors.title}</span>
-        }
+        <input id="title"/>
+
       </div>
       <div className="input-container">
         <label htmlFor="category">Category</label>
-        <select id="category" name="category" value={expense.category} onChange={handleChange}>
+        <select id="category">
           <option value="" hidden>
             Select Category
           </option>
@@ -55,12 +25,10 @@ export default function ExpenseForm({ setExpenses }) {
           <option value="education">Education</option>
           <option value="medicine">Medicine</option>
         </select>
-        {errors.category && <span style={{color:'red'}}>{errors.category}</span>}
       </div>
       <div className="input-container">
         <label htmlFor="amount">Amount</label>
-        <input id="amount" name="amount" value={expense.amount} onChange={handleChange} />
-        {errors.amount && <span style={{color:'red'}}>{errors.amount}</span>}
+        <input id="amount"/>
       </div>
       <button type="submit" className="add-btn">
         Add
