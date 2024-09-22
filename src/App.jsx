882cf8f51012cb071e9cf8c266a046861765fe92
tanvirtbf <tanver018765@gmail.com>
2024-09-22 22:16:30 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,16 @@ import expenseData from "./expenseData";
 function App() {
   const [expenses,setExpenses] = useState(expenseData)
 
+  const handleSubmit = (title,category,amount) => {
+    setExpenses((prevState)=> [...prevState, {id:crypto.randomUUID(), title, category, amount}])
+  }
+
   return (
     <>
       <main>
         <h1>Track Your Expense</h1>
         <div className="expense-tracker">
-          <ExpenseForm />
+          <ExpenseForm handleSubmit={handleSubmit} />
           <ExpenseTable expenses={expenses} />
         </div>
       </main>
--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -1,19 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
-const ExpenseForm = () => {
+const ExpenseForm = ({handleSubmit}) => {
+  const [title,setTitle] = useState('')
+  const [category, setCategory] = useState('')
+  const [amount,setAmount] = useState('')
+
+  function submitForm(e){
+    e.preventDefault();
+    handleSubmit(title,category,amount)
+  } 
+
   return (
-    <form className="expense-form">
+    <form className="expense-form" onSubmit={(e)=> submitForm(e)}>
       <div className="input-container">
         <label htmlFor="title">Title</label>
-        <input id="title" />
+        <input id="title" value={title} onChange={(e)=> setTitle(e.target.value)} />
       </div>
       <div className="input-container">
         <label htmlFor="category">Category</label>
-        <input id="category" />
+        <select id="category" value={category} onChange={(e)=> setCategory(e.target.value)} >
+          <option value="" hidden>Select Category</option>
+          <option value="grocery">Grocery</option>
+          <option value="clothes">Clothes</option>
+          <option value="bills">Bills</option>
+          <option value="education">Education</option>
+          <option value="medicine">Medicine</option>
+        </select>
       </div>
       <div className="input-container">
         <label htmlFor="amount">Amount</label>
-        <input id="amount" />
+        <input id="amount" value={amount} onChange={(e)=> setAmount(e.target.value)} />
       </div>
       <button className="add-btn">Add</button>
     </form>
