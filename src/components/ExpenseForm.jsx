0f9b1212922b63c74ae1f8056331107426905374
tanvirtbf <tanver018765@gmail.com>
2024-09-25 17:20:17 +0600

    start validation alone

--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -7,28 +7,9 @@ const ExpenseForm = ({ setExpenses }) => {
     amount: "",
   });
 
-  const [errors,setErrors] = useState({})
-
-  function validate(formData){
-    const errorsData = {}
-    if(!formData.title){
-      errorsData.title = "Title is Required!"
-    }
-    if(!formData.category){
-      errorsData.category = "Category is Required!"
-    }
-    if(!formData.amount){
-      errorsData.amount = "Amount is Required!"
-    }
-    setErrors(errorsData)
-    return errorsData
-  }
 
   function submitForm(e) {
     e.preventDefault();
-
-    const validateResult = validate(expense)
-    if(Object.keys(validateResult).length) return 
 
     setExpenses((prevState) => [
       ...prevState,
@@ -43,11 +24,6 @@ const ExpenseForm = ({ setExpenses }) => {
       [e.target.name]: e.target.value,
     }));
 
-    setErrors((prevState)=> ({
-      ...prevState,
-      [e.target.name]: '',
-    }))
-
   }
 
   return (
@@ -60,7 +36,6 @@ const ExpenseForm = ({ setExpenses }) => {
           value={expense.title}
           onChange={commonFunction}
         />
-        <span className="error">{errors.title}</span>
       </div>
       <div className="input-container">
         <label htmlFor="category">Category</label>
@@ -79,7 +54,6 @@ const ExpenseForm = ({ setExpenses }) => {
           <option value="education">Education</option>
           <option value="medicine">Medicine</option>
         </select>
-        <span className="error">{errors.category}</span>
       </div>
       <div className="input-container">
         <label htmlFor="amount">Amount</label>
@@ -89,7 +63,6 @@ const ExpenseForm = ({ setExpenses }) => {
           value={expense.amount}
           onChange={commonFunction}
         />
-        <span className="error">{errors.amount}</span>
       </div>
       <button className="add-btn">Add</button>
     </form>
