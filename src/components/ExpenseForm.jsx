f6c4721fdc0dfd0a4397d1e816e9a96e676c4c42
tanvirtbf <tanver018765@gmail.com>
2024-09-25 17:29:23 +0600

    complete validation

--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -7,9 +7,28 @@ const ExpenseForm = ({ setExpenses }) => {
     amount: "",
   });
 
+  const [errors,setErrors] = useState({})
+
+  function validate(form){
+    let formErrors = {}
+    if(form.title===''){
+      formErrors.title = 'Title is Required!'
+    }
+    if(form.category===''){
+      formErrors.category = 'Category is Required!'
+    }
+    if(form.amount===''){
+      formErrors.amount = 'Amount is Required!'
+    }
+    setErrors(formErrors)
+    return formErrors
+  }
 
   function submitForm(e) {
     e.preventDefault();
+
+    const validationResult = validate(expense)
+    if(Object.keys(validationResult).length) return 
 
     setExpenses((prevState) => [
       ...prevState,
@@ -36,6 +55,7 @@ const ExpenseForm = ({ setExpenses }) => {
           value={expense.title}
           onChange={commonFunction}
         />
+        <p className="error">{errors.title}</p>
       </div>
       <div className="input-container">
         <label htmlFor="category">Category</label>
@@ -54,6 +74,7 @@ const ExpenseForm = ({ setExpenses }) => {
           <option value="education">Education</option>
           <option value="medicine">Medicine</option>
         </select>
+        <p className="error">{errors.category}</p>
       </div>
       <div className="input-container">
         <label htmlFor="amount">Amount</label>
@@ -63,6 +84,7 @@ const ExpenseForm = ({ setExpenses }) => {
           value={expense.amount}
           onChange={commonFunction}
         />
+        <p className="error">{errors.amount}</p>
       </div>
       <button className="add-btn">Add</button>
     </form>
