6c232b20df88e0923591256ab6ac0fcc63401cdf
tanvirtbf <tanver018765@gmail.com>
2024-09-25 17:45:16 +0600

    update

--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -1,34 +1,16 @@
 import { useState } from "react";
-
+import { useForm } from 'react-hook-form'
 const ExpenseForm = ({ setExpenses }) => {
+
+  const {register, handleSubmit} = useForm()
+
   const [expense, setExpense] = useState({
     title: "",
     category: "",
     amount: "",
   });
 
-  const [errors,setErrors] = useState({})
-
-  function validate(form){
-    let formErrors = {}
-    if(form.title===''){
-      formErrors.title = 'Title is Required!'
-    }
-    if(form.category===''){
-      formErrors.category = 'Category is Required!'
-    }
-    if(form.amount===''){
-      formErrors.amount = 'Amount is Required!'
-    }
-    setErrors(formErrors)
-    return formErrors
-  }
-
-  function submitForm(e) {
-    e.preventDefault();
-
-    const validationResult = validate(expense)
-    if(Object.keys(validationResult).length) return 
+  function onSubmit(form) {
 
     setExpenses((prevState) => [
       ...prevState,
@@ -42,30 +24,24 @@ const ExpenseForm = ({ setExpenses }) => {
       ...prevState,
       [e.target.name]: e.target.value,
     }));
-
-    setErrors((prevState)=> ({
-      ...prevState, 
-      [e.target.name]: '',
-    }))
   }
 
   return (
-    <form className="expense-form" onSubmit={(e) => submitForm(e)}>
+    <form className="expense-form" onSubmit={handleSubmit(onSubmit)}>
       <div className="input-container">
         <label htmlFor="title">Title</label>
         <input
+          {...register('title', {required:true})}
           id="title"
-          name="title"
           value={expense.title}
           onChange={commonFunction}
         />
-        <p className="error">{errors.title}</p>
       </div>
       <div className="input-container">
         <label htmlFor="category">Category</label>
         <select
+          {...register('category',{required:true})}
           id="category"
-          name="category"
           value={expense.category}
           onChange={commonFunction}
         >
@@ -78,17 +54,15 @@ const ExpenseForm = ({ setExpenses }) => {
           <option value="education">Education</option>
           <option value="medicine">Medicine</option>
         </select>
-        <p className="error">{errors.category}</p>
       </div>
       <div className="input-container">
         <label htmlFor="amount">Amount</label>
         <input
+          {...register('amount', {required:true})}
           id="amount"
-          name="amount"
           value={expense.amount}
           onChange={commonFunction}
         />
-        <p className="error">{errors.amount}</p>
       </div>
       <button className="add-btn">Add</button>
     </form>
